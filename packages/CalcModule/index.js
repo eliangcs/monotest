a70e24a4f14d727ce5eaa14283dd0333214a97af
Chang-Hung Liang <eliang.cs@gmail.com>
2018-12-21 14:48:35 +0800

    Initial commit

---
+++ packages/CalcModule/index.js
@@ -0,0 +1,5 @@
+const additionModule = require('AdditionModule');
+const subtractionModule = require('SubtractionModule');
+
+console.log('4 + 5 = ' + additionModule.add(4, 5));
+console.log('5 + 4 = ' + subtractionModule.subtract(5, 4));
