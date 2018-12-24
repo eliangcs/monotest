581273b74d13ac359732353ba8e8655d02952e72
Chang-Hung Liang <eliang.cs@gmail.com>
2018-12-24 22:07:45 +0800

    update

--- packages/AdditionModule/test/test.js
+++ packages/AdditionModule/test/test.js
@@ -10,4 +10,8 @@ describe('Addition', () => {
   it('Two negative integers', () => {
     assert.equal(add(-1, -2), -3);
   });
+
+  it('Two floats', () => {
+    assert.equal(add(1.0, 2.0), 3.0);
+  });
 });
