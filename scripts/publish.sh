a3af53d2eb6d26c4ca5ceece9918810739570900
Chang-Hung Liang <eliang.cs@gmail.com>
2018-12-25 15:20:37 +0800

    add info

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -35,5 +35,9 @@ NPMRC_PATH="$HOME/.npmrc"
 NPM_REGISTRY_NO_PROTO=`echo $NPM_REGISTRY | sed -E 's/^https?://'`
 echo "$NPM_REGISTRY_NO_PROTO/:_authToken=\"$NPM_TOKEN\"" > $NPMRC_PATH
 
+pwd
+
+ls ./node_modules/.bin
+
 LERNA="./node_modules/.bin/lerna"
 $LERNA run --stream --scope $PKG_NAME publish -- --registry $NPM_REGISTRY
